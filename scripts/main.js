80d13c5d754132e0d01a0df5c88c00e249033e33
nznmvs <nznmvs@gmail.com>
2020-08-27 15:38:46 +0300

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const food = {
+  'Meat': {
+    'Pork': {},
+    'Beef': {},
+  },
+
+  'Fruit': {
+    'Red': {
+      'Cherry': {},
+      'Strawberry': {},
+    },
+    'Yellow': {
+      'Banana': {},
+      'Pineapple': {},
+    },
+
+  },
+};
+
+const tree = document.querySelector('#tree');
+
+function createTree(element, data) {
+  const list = document.createElement('ul');
+
+  for (const key in data) {
+    const listItem = document.createElement('li');
+
+    listItem.textContent = key;
+    list.append(listItem);
+
+    if (typeof data[key] === 'object' && Object.keys(data[key]).length) {
+      createTree(list, data[key]);
+    }
+  }
+
+  element.append(list);
+}
+
+createTree(tree, food);
